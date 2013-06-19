b59c5e09f0c1931e4ac6eb9b918aa2305e7b4e32
Josef Patoprsty <seppi@josefnpat.com>
2013-06-19 02:12:06 -0400

    Add debug option

--- server/server.lua
+++ server/server.lua
@@ -8,16 +8,19 @@ local ip, port = server:getsockname()
 
 print("Server hosted on port " .. port)
 
+debug = false
 debug_cycles = 0
 debug_cycles_max = 100
 
 local clients = {}
 
 while 1 do
-  debug_cycles = debug_cycles + 1
-  if debug_cycles > debug_cycles_max then
-    debug_cycles = 0
-    print("Performed "..debug_cycles_max)
+  if debug then
+    debug_cycles = debug_cycles + 1
+    if debug_cycles > debug_cycles_max then
+      debug_cycles = 0
+      print("Performed "..debug_cycles_max)
+    end
   end
   
   server:settimeout(0.01)
@@ -25,34 +28,52 @@ while 1 do
   if client then
     client:settimeout(0.01)
     table.insert(clients,client)
+    print("User connected.")
   else
-    --print("accept error:"..error)
+    if debug then
+      print("accept error:"..error)
+    end
   end
   
-  print("clients:")
-  for clienti,client in ipairs(clients) do
-    print(clienti,client)
+  if debug then
+    print("clients:")
+    for clienti,client in ipairs(clients) do
+      print(clienti,client)
+    end
   end
   
   for clienti,client in ipairs(clients) do
-    print(clienti.." client:receive start")
+    if debug then
+      print(clienti.." client:receive start")
+    end
     local line,error = client:receive()
-    print(clienti.." client:receive stop")
+    if debug then
+      print(clienti.." client:receive stop")
+    end
     if error then
       if error == "closed" then
         client:close()
         table.remove(clients,clienti)
+        print("User disconnected.")
       end
-      print("receive error:"..error)
+      if debug then
+        print("receive error:"..error)
+      end
     else
       if pcall(function() json.decode(line) end) then
         data = json.decode(line)
-        print(clienti.." client:send start")
+        if debug then
+          print(clienti.." client:send start")
+        end
         client:send(json.encode({test="Hello world."}).."\n")
-        print(clienti.." client:send stop")
-        print("receive:"..line)
+        if debug then
+          print(clienti.." client:send stop")
+          print("receive:"..line)
+        end
       else
-        print("json.decode() failed")
+        if debug then
+          print("json.decode() failed")
+        end
       end
     end
     
