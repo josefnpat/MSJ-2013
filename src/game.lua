1901503e67a283aed4fecb8c8b6c5a2474b32eff
Josef Patoprsty <seppi@josefnpat.com>
2013-06-21 19:53:33 -0400

    Adding window object Moving cameraMove out of the load function, since it's a gloabl Using cameraMove with dt Correcting the tile draw to work properly with the updated camera config using the setScale command instead of overriding the library correcting the setDrawRange to only draw the screen updating the selection math to orient proper with the updated configurations

--- src/game.lua
+++ src/game.lua
@@ -1,12 +1,19 @@
 require "camera"
 local game = {}
 
+window = {}
+window.padding = 32
+window.x = 0
+window.y = 0
+window.w = love.graphics.getWidth()
+window.h = love.graphics.getHeight()
 selection = {}
 selection.x = 0
 selection.y = 0
 mouseLoc = {}
 mouseLoc.x = 0
 mouseLoc.y = 0
+cameraMove = 500
 
 function game:init()
   local AdvTiledLoader = require("AdvTiledLoader.Loader")
@@ -19,15 +26,17 @@ function game:init()
   
   AdvTiledLoader.path = "maps/"
   map = AdvTiledLoader.load("map.tmx")
-  map:setDrawRange(mapX, mapY, map.width * map.tileWidth, map.height * map.tileHeight)
-  
-  cameraMove = 2 * 4.3
+  map:setDrawRange(window.x,window.y,window.w,window.h)  
+  camera:setScale(0.5,0.5)
 end
 
 function game:draw()
   camera:set()
   map:draw()
-  love.graphics.draw(selection_tile, math.floor(selection.x * map.tileWidth), math.floor(selection.y * map.tileHeight))
+  love.graphics.draw(selection_tile,
+    math.floor( (selection.x-1) * map.tileWidth),
+    math.floor( (selection.y-1) * map.tileHeight)
+  )
   camera:unset()
   love.graphics.print("Tile X: " .. selection.x .. ", Mouse X: " .. mouseLoc.x, 20, 10)
   love.graphics.print("Tile Y: " .. selection.y .. ", Mouse Y: " .. mouseLoc.y, 20, 30)
@@ -36,21 +45,21 @@ end
 function game:update(dt)
 
   if love.keyboard.isDown("d") then
-    camera.x = camera.x + cameraMove
+    camera.x = camera.x + cameraMove*dt
   end
   if love.keyboard.isDown("a") then
-    camera.x = camera.x - cameraMove
+    camera.x = camera.x - cameraMove*dt
   end
   if love.keyboard.isDown("w") then
-    camera.y = camera.y - cameraMove
+    camera.y = camera.y - cameraMove*dt
   end
   if love.keyboard.isDown("s") then
-    camera.y = camera.y + cameraMove
+    camera.y = camera.y + cameraMove*dt
   end
   
   mouseLoc.x, mouseLoc.y = love.mouse.getPosition( ) 
-  selection.x =  math.ceil((mouseLoc.x * camera.sx + (camera.x - 64)) / map.tileWidth) + 1
-  selection.y =  math.ceil((mouseLoc.y * camera.sy + (camera.y - 64)) / map.tileHeight) + 1
+  selection.x =  math.ceil((mouseLoc.x * camera.sx + (camera.x)) / map.tileWidth)
+  selection.y =  math.ceil((mouseLoc.y * camera.sy + (camera.y)) / map.tileHeight)
   
 end
 
