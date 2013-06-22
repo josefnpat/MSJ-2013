c8123be0ced4a5708a46e9cb4c041121491c903f
Josef Patoprsty <seppi@josefnpat.com>
2013-06-22 00:57:19 -0400

    Have keyboard override mouse, but avoid mouse from doubling scroll speed.

--- src/game.lua
+++ src/game.lua
@@ -43,31 +43,38 @@ function game:update(dt)
 
   love.graphics.setCaption(love.timer.getFPS())
 
+  local cam_move_by_key = false
   if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
     camera.x = camera.x + cameraMove*dt
+    cam_move_by_key = true    
   end
   if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
     camera.x = camera.x - cameraMove*dt
+    cam_move_by_key = true    
   end
   if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
     camera.y = camera.y - cameraMove*dt
+    cam_move_by_key = true    
   end
   if love.keyboard.isDown("s") or love.keyboard.isDown("down") then
     camera.y = camera.y + cameraMove*dt
+    cam_move_by_key = true    
   end
 
-  mx,my = love.mouse.getPosition()
-  if mx < love.graphics.getWidth()*mouseScroll then
-    camera.x = camera.x - cameraMove*dt
-  elseif mx > love.graphics.getWidth()*(1-mouseScroll) then
-    camera.x = camera.x + cameraMove*dt
+  if not cam_move_by_key then
+    mx,my = love.mouse.getPosition()
+    if mx < love.graphics.getWidth()*mouseScroll then
+      camera.x = camera.x - cameraMove*dt
+    elseif mx > love.graphics.getWidth()*(1-mouseScroll) then
+      camera.x = camera.x + cameraMove*dt
+    end
+    if my < love.graphics.getHeight()*mouseScroll then
+      camera.y = camera.y - cameraMove*dt
+    elseif my > love.graphics.getHeight()*(1-mouseScroll) then
+      camera.y = camera.y + cameraMove*dt
+    end
   end
-  if my < love.graphics.getHeight()*mouseScroll then
-    camera.y = camera.y - cameraMove*dt
-  elseif my > love.graphics.getHeight()*(1-mouseScroll) then
-    camera.y = camera.y + cameraMove*dt
-  end
-
+  
   if camera.x < 0 then
     camera.x = 0
   end
