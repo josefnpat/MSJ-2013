b42df6d0610ae81809d033e58f15962cbfe3ca52
Josef Patoprsty <seppi@josefnpat.com>
2013-06-23 04:29:38 -0400

    reducing random amount, prepping mapq

--- server/server.lua
+++ server/server.lua
@@ -18,6 +18,7 @@ function servercache.user.init(clientid)
     servercache.user.data[clientid].money = 2000
     servercache.user.data[clientid].publicid = servercache.user.publicid
     servercache.user.publicid = servercache.user.publicid + 1
+    servercache.user.data[clientid].last_mapq_update = socket.gettime()
   end
 end
 
@@ -40,7 +41,7 @@ for y = 1,servercache.map.y do
   for x = 1,servercache.map.x do
     -- TODO: have this data load from the collision layer from src/maps/map.tmx
     servercache.map.data[y][x] = {tile=0} -- open space
-    if math.random(1,10) == 1 or (x==1 and y == 1) or (x == 64 and y == 64) then
+    if math.random(1,100) == 1 or (x==1 and y == 1) or (x == 64 and y == 64) then
       servercache.map.data[y][x].tile = math.random(1,#servercache.buildings.data)
       servercache.map.data[y][x].owner = math.random(1,10)
     end
