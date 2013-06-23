ee5e5cd3172f8b9b3244d6977f9885a85c7ef8a0
Josef Patoprsty <seppi@josefnpat.com>
2013-06-23 15:56:23 -0400

    fixed

--- src/colorgen.lua
+++ src/colorgen.lua
@@ -5,7 +5,7 @@ colorgen._colors = {}
 for r = 1,0,-1 do
   for g = 1,0,-1 do
     for b = 1,0,-1 do
-      local color = {255*r/2,255*g/2,255*b/2}
+      local color = {255*r,255*g,255*b}
        table.insert(colorgen._colors,color)
     end
   end
